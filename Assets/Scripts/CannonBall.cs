b721b98c48fa5c59e3b31c2483ae473de5e8a76a
ficasmica <54986618+ficasmica@users.noreply.github.com>
2020-02-09 23:58:50 +0100

    main

---
+++ Assets/Scripts/CannonBall.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBall : MonoBehaviour
+{
+    public float speed;
+
+    void Start(){
+    }
+
+    void FixedUpdate(){
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    public CannonBall cannonBall;
+    public Transform rightGun;
+    public Transform leftGun;
+
+    public void ShootRight(){
+        CannonBall cannonBallClone = Instantiate(cannonBall, rightGun.position, rightGun.rotation);
+    }
+
+    public void ShootLeft(){
+        CannonBall cannonBallClone = Instantiate(cannonBall, leftGun.position, leftGun.rotation);
+    }
+}
